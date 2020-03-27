e6f1f96e350453af2f8f4551be2c2637b111e9d5
Kerby Jean <kerby.jean@hotmail.fr>
2020-03-27 11:07:07 -0400

    First commit

---
+++ Calorie Tracker/Calorie Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Tracker
+//
+//  Created by Kerby Jean on 3/27/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
